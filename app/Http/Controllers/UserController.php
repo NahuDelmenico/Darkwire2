c3bac0678a20b50734b7de781842f0a261829b8f
8auti <bautistasilvalosilla@gmail.com>
2025-08-03 14:09:02 -0300

    Quickfix libreria dupeada

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\Console\Input\Input;
-use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
